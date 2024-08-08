a0d5117234413a662916542acb0259f61b477394
koog7 <k.ognev709@mail.com>
2024-08-08 15:35:42 +0600

    Настроен сервер для возможности делать на него запрсоы , подключен cors

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import cryptoRouter from "./routers/crypto";
+import cors from 'cors';
 
 const app = express();
 const port = 8000;
 
+app.use(cors())
 app.use(express.json())
 app.use('/', cryptoRouter)
+
 app.listen(port, () => {
     console.log('Server starter : http://127.0.0.1:' + port);
 });
